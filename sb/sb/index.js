f889def0cf7ae30169740b5d0ed42272ec32ca6b
Chloreos <Chloreosspades@gmail.com>
2025-03-15 23:53:34 -0500

    added.. everything

---
+++ sb/sb/index.js
@@ -0,0 +1,33 @@
+const { Client, MessageEmbed, TextChannel, GuildChannel, Channel, Message } = require('discord.js-selfbot-v13')
+
+const client = new Client({
+    checkUpdate: false
+});
+
+client.on('ready', async () => {
+    console.log(`${client.user.username} is ready!`);
+});
+
+const token = "" //put account token here
+reportChannelId = "837928153774424084"
+coinChannelId = "840431174845071401"
+modRoleId = "839237255158956042"
+pylonBotId = "270148059269300224"
+
+client.on("messageReactionAdd", async (reaction) => {
+  try {
+  reactorId = JSON.parse(JSON.stringify(reaction.users["_cache"]))[0].id
+  if (reaction.emoji.name == "✅" && reaction.message.author.id == `${pylonBotId}` && reaction.message.channelId == reportChannelId && reaction.message.guild.members.cache.get(reactorId)["_roles"].includes(modRoleId)) {
+    args = reaction.message.content
+    args = args.replaceAll(/<|>|@/g, '')
+    args = args.split(", ")
+    for (let i = 0; i < args.length; i++) {
+      setTimeout(()=>{
+        client.channels.cache.get(coinChannelId).send(`$add-money <@${args[i]}> 100`)
+      }, 15000*(i+1))
+    }
+  }
+} catch (err) {reaction.message.channel.send('`There was an error.`')}
+})
+
+client.login(`${token}`)
