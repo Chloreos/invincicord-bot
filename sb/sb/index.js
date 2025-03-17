4d1c4f43d3020615c74eff800933ec38f578ae84
Chloreos <Chloreosspades@gmail.com>
2025-03-17 18:58:54 -0500

    Update index.js

--- sb/sb/index.js
+++ sb/sb/index.js
@@ -13,22 +13,34 @@ reportChannelId = "837928153774424084"
 coinChannelId = "840431174845071401"
 modRoleId = "839237255158956042"
 pylonBotId = "270148059269300224"
+coinStatus = false
 
 client.on("messageReactionAdd", async (reaction) => {
   try {
   if (reaction.message.channelId == reportChannelId) {
   reactorId = JSON.parse(JSON.stringify(reaction.users["_cache"]))[0].id
   if (reaction.emoji.name == "✅" && reaction.message.author.id == `${pylonBotId}` && reaction.count == '1' && reaction.message.guild.members.cache.get(reactorId)["_roles"].includes(modRoleId)) {
+    if (coinStatus == true) {
+      client.channels.cache.get(reportChannelId).send('`Currently distributing coins.`')
+    } else {
     args = reaction.message.content
     args = args.replaceAll(/<|>|@|!/g, '')
     args = args.split(", ")
+    coinStatus = true
+    i2 = 0
     for (let i = 0; i < args.length; i++) {
       setTimeout(()=>{
         client.channels.cache.get(coinChannelId).send(`$add-money <@${args[i]}> 100`)
+        i2++
+        if (i2 == args.length) {
+          coinStatus = false
+          client.channels.cache.get(reportChannelId).send('`Done distributing coins`')
+        }
       }, 10000*(i))
     }
   }
   }
+}
 } catch (err) {client.channels.cache.get(reportChannelId).send('`There was an error`')}
 })
 
